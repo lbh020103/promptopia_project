0a2f674faa29859ea651f51021e1900727310aa8
hbaoDX <hoaibao.020103@gmail.com>
2023-06-01 11:28:59 +0800

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import '@styles/globalstyles.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <Provider>
+        <body
+            suppressHydrationWarning={true}
+        >
+            <div className='main'>
+                <div className='gradient' />
+            </div>
+
+            <main className='app'>
+                <Nav />
+                {children}
+            </main>
+        </body>
+        </Provider>
+    </html>
+  )
+}
+
+export default RootLayout
